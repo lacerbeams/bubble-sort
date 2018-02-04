70a49bf2aca55874c67151963cc2d4ff3a4b46f0
Lacey Boblak <laceyboblak@Laceys-MacBook-Pro.local>
2018-02-04 00:01:49 -0800

    fojfidsifosd

--- main.js
+++ main.js
@@ -1,23 +1,98 @@
 console.log('hello from main.js!');
 
+var nums = [];
+
+var i = 9;
+
+var rand;
+
+var row;
+
+
 function randomNumber(max) {
 
 	return Math.floor(Math.random() * Math.floor(max));
 
 }
 
+
 function makeBar(max) {
 
-	var rand = randomNumber(max);
+	rand = randomNumber(max);
 
-	$('table').append('<tr><td colspan=' + rand / 5 + ' style="height: 20px; background-color: #684791; margin: 10px;"></td></tr>');
+	$('table').append('<tr id= "' + rand + '"><td colspan=' + rand / 5 + ' style="height: 20px; background-color: #684791; margin: 10px;"></td></tr>');
 
 	console.log(rand);
 
-}
-
-for (var i = 0; i < 10; i++) {
-
-	makeBar(100);
+	nums.unshift(rand);
 
 }
+
+
+$('#shuffle').click(function() {
+
+	$('table').html(`<colgroup>
+		<col width="5%"><col width="5%">
+        <col width="5%"><col width="5%">
+        <col width="5%"><col width="5%">
+        <col width="5%"><col width="5%">
+        <col width="5%"><col width="5%">
+        <col width="5%"><col width="5%">
+        <col width="5%"><col width="5%">
+        <col width="5%"><col width="5%">
+        <col width="5%"><col width="5%">
+        <col width="5%"><col width="5%">
+		</colgroup>`)
+
+	for (var j = 0; j < 10; j++) {
+
+		makeBar(100);
+
+	}
+
+});
+
+
+$('#step').click(function(){
+
+	$('#bars').children().css('background-color', '684791');
+
+	if (i == 0) {
+
+		i = 10;
+
+	} 
+
+	if (nums[i] > nums[i - 1]) {
+
+		var temp = nums[i];
+
+		nums[i] = nums[i - 1];
+
+		nums[i - 1] = temp;
+
+		rowOne = $('#'+nums[i]);
+
+		rowTwo = $('#'+nums[i - 1]);
+
+		rowOne.first().css("background-color", "#cfa3ff");
+
+		rowTwo.first().css("background-color", "#a984ce");
+
+		console.log(rowOne, rowTwo);
+
+		rowTwo.insertBefore(rowTwo.prev());
+
+	} 
+
+	i --;
+
+	return nums;
+
+});
+
+
+
+
+
+
